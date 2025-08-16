2167bc7fbe47dba6daf0429d182508bf19273f4d
lucasedu191 <lucasatletico@hotmail.com>
2025-08-16 11:21:36 -0300

    Revert "ajuste botao de cotação pt3"
    This reverts commit b2fb4ec36fafb67a3fbda7f8fc562d07ba4765c7.

--- modelos/partes/botao-cotacao-popup.php
+++ modelos/partes/botao-cotacao-popup.php
@@ -1,6 +1,10 @@
 <?php
+/**
+ * Botão que abre o modal de cotação.
+ * Usa $GLOBALS['a'] com: produto_id, produto_titulo, rotulo, classe, nonce
+ */
 $a = isset($GLOBALS['a']) ? $GLOBALS['a'] : [];
-$rotulo         = isset($a['rotulo']) && $a['rotulo'] ? $a['rotulo'] : 'Gerar cotação';
+$rotulo         = isset($a['rotulo']) ? $a['rotulo'] : 'Gerar cotação';
 $classe         = isset($a['classe']) ? $a['classe'] : 'bvgn-btn bvgn-btn--primario';
 $produto_id     = isset($a['produto_id']) ? (int)$a['produto_id'] : 0;
 $produto_titulo = isset($a['produto_titulo']) ? $a['produto_titulo'] : '';
@@ -12,6 +16,7 @@ $nonce          = isset($a['nonce']) ? $a['nonce'] : '';
   <?php echo esc_html($rotulo); ?>
 </button>
 <?php
+// Inclui o modal apenas uma vez por página
 if ( ! defined('BVGN_MODAL_INCLUDED') ) {
   define('BVGN_MODAL_INCLUDED', true);
   $modal_data = [
