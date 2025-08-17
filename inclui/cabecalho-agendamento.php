d204488a94263f03d5e5ebf54cc05e128953c5df
lucasedu191 <lucasatletico@hotmail.com>
2025-08-17 19:22:10 -0300

    ajuste calendario cabecalho

--- inclui/cabecalho-agendamento.php
+++ inclui/cabecalho-agendamento.php
@@ -4,8 +4,8 @@
  */
 ?>
 <div class="bvgn-agendamento-form">
-  <input type="date" id="bvgn-inicio" placeholder="Data de início">
-  <input type="date" id="bvgn-fim" placeholder="Data de fim">
+  <input type="text" id="bvgn-inicio" placeholder="Data de início">
+  <input type="text" id="bvgn-fim" placeholder="Data de fim">
   <input type="hidden" id="bvgn-local" value="Sede">
   <button id="bvgn-buscar-grupos">Buscar carros</button>
 </div>
