97e8178ac0c1c204cc78bdb5efbc2a8edf09654a
lucasedu191 <lucasatletico@hotmail.com>
2025-08-19 21:50:53 -0300

    ajuste bloco de serviços

--- modelos/partes/taxas.php
+++ modelos/partes/taxas.php
@@ -9,17 +9,24 @@ $taxas = BVGN_IntegracoesPT::obter_taxas_para_produto($a['produto_id']);
   <?php foreach($taxas as $t): ?>
     <?php
       $icone = !empty($t['icone']) ? $t['icone'] : 'assets/svg/passos03.svg';
+      $rotulo = $t['rotulo'];
+
+      // quebra "(diaria)" para uma nova linha, se existir
+      if (str_contains($rotulo, '(diaria)')) {
+        $rotulo = str_replace('(diaria)', '<br><small>(diaria)</small>', $rotulo);
+      }
     ?>
     <label class="bvgn-taxa">
       <input type="checkbox"
              data-preco="<?php echo esc_attr($t['preco']); ?>"
              data-rotulo="<?php echo esc_attr($t['rotulo']); ?>" />
       <span class="lbl">
-        <img class="bvgn-icon" src="<?php echo BVGN_URL . $icone; ?>" alt="">
-        <span class="texto"><?php echo esc_html($t['rotulo']); ?></span>
+        <img class="bvgn-icon" src="<?php echo BVGN_URL . esc_attr($icone); ?>" alt="">
+        <span class="texto"><?php echo $rotulo; ?></span>
         <span class="preco">R$ <?php echo number_format($t['preco'], 2, ',', '.'); ?></span>
         <span class="botao-fake">Selecionar</span>
       </span>
     </label>
   <?php endforeach; ?>
 </div>
+
