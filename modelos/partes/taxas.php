53e7abac69a7d543e9baaf21ad5ea1d2c4df4eb3
lucasedu191 <lucasatletico@hotmail.com>
2025-08-20 22:59:09 -0300

    ajuste versao para exibir resumo

--- modelos/partes/taxas.php
+++ modelos/partes/taxas.php
@@ -6,29 +6,33 @@ $taxas = BVGN_IntegracoesPT::obter_taxas_para_produto($a['produto_id']);
 
   <?php include __DIR__ . '/taxa-fixa-mensal.php'; ?>
 
-  <?php foreach($taxas as $t): ?>
-    <?php
-      $icone = !empty($t['icone']) ? $t['icone'] : 'assets/svg/passos03.svg';
-      $rotulo = $t['rotulo'];
+<?php foreach($taxas as $t): ?>
+  <?php
+    $icone = !empty($t['icone']) ? $t['icone'] : 'assets/svg/passos03.svg';
+    $rotulo = $t['rotulo'];
 
-      // substitui "(diaria)" e "(obrigatória)" para quebrar a linha
-      $rotulo = str_replace(
-        ['(diaria)', '(obrigatória)'],
-        ['<br><small>(diaria)</small>', '<br><small>(obrigatória)</small>'],
-        $rotulo
-      );
-    ?>
-    <label class="bvgn-taxa">
-      <input type="checkbox"
-             data-preco="<?php echo esc_attr($t['preco']); ?>"
-             data-rotulo="<?php echo esc_attr($t['rotulo']); ?>" />
-      <span class="lbl">
-        <img class="bvgn-icon" src="<?php echo BVGN_URL . esc_attr($icone); ?>" alt="">
-        <span class="texto"><?php echo $rotulo; ?></span>
-        <span class="preco">R$ <?php echo number_format($t['preco'], 2, ',', '.'); ?></span>
-        <span class="botao-fake">Selecionar</span>
-      </span>
-    </label>
-  <?php endforeach; ?>
+    // Detecta se é taxa obrigatória
+    $is_obrigatoria = stripos($rotulo, 'limpeza') !== false;
+
+    // Substitui partes do rótulo por <small>
+    $rotulo = str_replace(
+      ['(diaria)', '(obrigatória)'],
+      ['<br><small>(diaria)</small>', '<br><small>(obrigatória)</small>'],
+      $rotulo
+    );
+  ?>
+  <label class="bvgn-taxa">
+    <input type="checkbox"
+           data-preco="<?php echo esc_attr($t['preco']); ?>"
+           data-rotulo="<?php echo esc_attr($t['rotulo']); ?>"
+           <?php echo $is_obrigatoria ? 'checked' : ''; ?> />
+    <span class="lbl">
+      <img class="bvgn-icon" src="<?php echo BVGN_URL . esc_attr($icone); ?>" alt="">
+      <span class="texto"><?php echo $rotulo; ?></span>
+      <span class="preco">R$ <?php echo number_format($t['preco'], 2, ',', '.'); ?></span>
+      <span class="botao-fake">Selecionar</span>
+    </span>
+  </label>
+<?php endforeach; ?>
 </div>
 
