b78944ca976a34fd435fb94e48e8e177ac71b5c1
lucasedu191 <lucasatletico@hotmail.com>
2025-12-03 18:26:34 -0300

    ajuste versao

--- bv-grupo-novo.php
+++ bv-grupo-novo.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: BV Grupo Novo (Produto Paralelo)
  * Description: Página de produto paralela com shortcodes modulares (Diário/Mensal), taxas, agendamento, totais e cotação (HTML/PDF + WhatsApp).
- * Version: 9.9.81
+ * Version: 9.9.82
  * Author: Lucas
  * Update URI: https://github.com/Lucasedu191/bv-grupo-novo-git
  */
